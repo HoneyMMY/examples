8ff4cca2270fea6bec46233a712fadf10010f087
Big CoCo <2276067609@qq.com>
2016-08-29 21:17:24 +0800

    2016-8-29
    This is  an example for time-test.

---
+++ time_test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace time_test
+{
+    class Program
+    {
+        #region
+        static void Main( string[ ] args )
+        {
+            Stopwatch watch = new Stopwatch( );
+            //注释语句
+            int[ ] numbers = new int[1000000];
+            watch.Start( );
+         for (int i = 0; i < 25; i++) 
+         {
+             if ( numbers[i] > 0 )
+             numbers[i] = 0;
+         }
+         watch.Stop( );
+  
+        Console.WriteLine("Elapsed: {0}",watch.Elapsed);
+        Console.WriteLine("In milliseconds: {0}",watch.ElapsedMilliseconds);
+        Console.WriteLine( "In timer ticks: {0}", watch.ElapsedTicks );
+        Console.ReadLine( );
+        }
+       #endregion
+    }
+}
